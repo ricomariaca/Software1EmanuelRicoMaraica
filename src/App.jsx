81bb6799eb8ba95c310ba6b4325b0657305d1641
unknown <ricomariaca@gmail.com>
2024-03-08 14:57:44 -0500

    ADDcomponents folder and AddCategory.jsx component were created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import AddCategory from "./components/AddCategory"
+
 function App() {
-    
+
+
 
     return (
         <div>
             <h1>Buscador de gifs</h1>
+            <AddCategory />
         </div>
 
     )
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const AddCategory = () => {
+    return (
+        <h2> Categorias </h2>
+    );
+    
+}
+
+export default AddCategory;
